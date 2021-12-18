7f9b9c2f4a308e24d7d312f7e3a8d5e236190777
NoahGaggioli <96350538+NoahGaggioli@users.noreply.github.com>
2021-12-18 15:26:34 -0500

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,27 +1,41 @@
-var xd
+
 function setup() {
   createCanvas(400,400);
-  xd = createSprite(200,200,25,25);
+  background(51);
+  box = createSprite(200,200,30,30);
+
 }
 
 function draw() 
 {
-  background(30);
-  if(keyIsDown(RIGHT_ARROW)){
-    xd.x += 5;
+
+  // escribir el código para cambiar el color del fondo 
+  // rojo cuando se presiona RIGHT_ARROW (tecla de flecha derecha)
+  
+  
+
+  if (keyIsDown(LEFT_ARROW)) 
+  {
+    background("blue");
+    
   }
-  if(keyIsDown(LEFT_ARROW)){
-    xd.position.x -= 5;
+ 
+    if (keyIsDown(UP_ARROW)) 
+  {
+    background("yellow");
+   
   }
-  if(keyIsDown(UP_ARROW)){
-    xd.y -= 5;
+
+  if (keyIsDown(DOWN_ARROW)) 
+  {
+    background("green");
   }
-  if(keyIsDown(DOWN_ARROW)){
-    xd.y += 5;
+  if (keyIsDown(RIGHT_ARROW)) 
+  {
+    background("red");
   }
-drawSprites();
+
+  
+  drawSprites();
 }
-
-
-
 
